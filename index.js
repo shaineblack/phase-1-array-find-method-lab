d6d8925ce8d071c1eeb2e29eaccb8b03decddcc7
shaineblack <shaineblck@gmail.com>
2024-02-11 19:06:10 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+    const winningYear = array.find(obj => obj.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
